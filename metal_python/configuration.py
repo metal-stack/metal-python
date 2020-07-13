9cab4c56370b3d0ad5e168a3a51fa0b702486b31
auto-build <info@metal-stack.io>
2020-07-13 11:33:32 +0000

    Bump to version spec-corrections

--- metal_python/configuration.py
+++ metal_python/configuration.py
@@ -255,5 +255,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: v0.7.8".\
+               "SDK Package Version: spec-corrections".\
                format(env=sys.platform, pyversion=sys.version)
