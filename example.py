f983d631472412e51baef3c24d910211b4a1ab01
Gerrit <tirreg91@googlemail.com>
2021-06-09 10:32:14 +0200

    Example should now use nip.io.

--- example.py
+++ example.py
@@ -2,6 +2,6 @@ from metal_python.driver import Driver
 from metal_python.api.partition_api import PartitionApi
 
 # example works with the mini-lab!
-d = Driver("http://api.0.0.0.0.xip.io:8080/metal", None, "metal-admin")
+d = Driver("http://api.0.0.0.0.nip.io:8080/metal", None, "metal-admin")
 partition = PartitionApi(api_client=d.client)
 print(partition.list_partitions())
