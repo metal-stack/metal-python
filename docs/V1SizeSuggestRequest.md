b23d54b0dabd9ec12d5ea43c2b01dae846c22e58
auto-build <info@metal-stack.io>
2023-10-12 13:13:24 +0000

    Bump to version v0.24.3

---
+++ docs/V1SizeSuggestRequest.md
@@ -0,0 +1,10 @@
+# V1SizeSuggestRequest
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**machine_id** | **str** | machineID to retrieve size suggestion for | 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- test/test_v1_size_suggest_request.py
+++ test/test_v1_size_suggest_request.py
@@ -16,12 +16,12 @@ from __future__ import absolute_import
 import unittest
 
 import metal_python
-from metal_python.models.httperrors_http_error_response import HttperrorsHTTPErrorResponse  # noqa: E501
+from metal_python.models.v1_size_suggest_request import V1SizeSuggestRequest  # noqa: E501
 from metal_python.rest import ApiException
 
 
-class TestHttperrorsHTTPErrorResponse(unittest.TestCase):
-    """HttperrorsHTTPErrorResponse unit test stubs"""
+class TestV1SizeSuggestRequest(unittest.TestCase):
+    """V1SizeSuggestRequest unit test stubs"""
 
     def setUp(self):
         pass
@@ -29,10 +29,10 @@ class TestHttperrorsHTTPErrorResponse(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testHttperrorsHTTPErrorResponse(self):
-        """Test HttperrorsHTTPErrorResponse"""
+    def testV1SizeSuggestRequest(self):
+        """Test V1SizeSuggestRequest"""
         # FIXME: construct object with mandatory attributes with example values
-        # model = metal_python.models.httperrors_http_error_response.HttperrorsHTTPErrorResponse()  # noqa: E501
+        # model = metal_python.models.v1_size_suggest_request.V1SizeSuggestRequest()  # noqa: E501
         pass
 
 
